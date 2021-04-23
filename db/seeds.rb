cbf0f5e0427278928c6375777dd8f3158987fe77
Phil Haines <phil.haines92@gmail.com>
2021-04-23 12:16:02 -0600

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-community1 = Community.create(name: 'My House')
-community2 = Community.create(name: 'My Work')
-community3 = Community.create(name: 'My Car')
+community1 = Community.create(name: 'House')
+community2 = Community.create(name: 'Work')
+community3 = Community.create(name: 'School')
 
 user1 = User.create(name: "Phil", email: "email", username:"Phil", password:"1234", pin: 1234, admin: true)
 user2 = User.create(name: "David", email: "email", username:"David", password:"abcd", pin: 4567, admin: false)
 
-task1 = Task.create(community: community1, title: "Title1", description: "Task 1", priority: "Medium", assigned: false, )
-task2 = Task.create(community: community2, title: "Title2", description: "Task 2", priority: "Medium", assigned: false, )
-task3 = Task.create(community: community1, title: "Title3", description: "Task 3", priority: "Medium", assigned: false, )
-task4 = Task.create(community: community1, title: "Title4", description: "Task 4", priority: "High", assigned: false, )
+task1 = Task.create(community: community1, title: "Wash Clothes", description: "Please make sure to use the good soap", priority: "Medium", assigned: false, )
+task2 = Task.create(community: community2, title: "File the TPS reports", description: "Do not omit the cover page", priority: "Low", assigned: false, )
+task3 = Task.create(community: community1, title: "Clean Fireplace", description: "Make sure to wear a mask!!!", priority: "Medium", assigned: false, )
+task4 = Task.create(community: community2, title: "Find my stapler", description: "I think that Jim took it again", priority: "High", assigned: false, )
 
 membership1 = Membership.create(user: user1, community: community1)
-membership2 = Membership.create(user: user2, community: community2)
-
-user_task1 = UserTask.create(user: user1, task: task1)
-user_task2 = UserTask.create(user: user1, task: task2)
-user_task3 = UserTask.create(user: user2, task: task3)
+membership2 = Membership.create(user: user1, community: community2)
+membership3 = Membership.create(user: user1, community: community3)
 
 puts "seeded"
